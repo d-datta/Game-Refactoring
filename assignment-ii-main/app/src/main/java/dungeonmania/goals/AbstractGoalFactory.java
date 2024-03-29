d05c128c22cdafb5e59d8b51729ae3cbcb1c0a24
Debargha Datta <debargha2002debu@gmail.com>
2024-03-29 20:50:55 +0530

    deleted duplicate files

--- assignment-ii-main/app/src/main/java/dungeonmania/goals/AbstractGoalFactory.java
+++
@@ -1,7 +0,0 @@
-package dungeonmania.goals;
-
-import org.json.JSONObject;
-
-public interface AbstractGoalFactory {
-    Goal createGoal(JSONObject jsonGoal, JSONObject config);
-}
--- assignment-ii-main/app/src/main/java/dungeonmania/goals/Goal.java
+++
@@ -1,8 +0,0 @@
-package dungeonmania.goals;
-
-import dungeonmania.Game;
-
-public interface Goal {
-    boolean achieved(Game game);
-    String toString(Game game);
-}
